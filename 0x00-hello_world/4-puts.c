448f536ce539ae6a727b2368226f1ceaf6a5c66b
Eli Amovure <eliamovure@gmail.com>
2022-10-17 12:40:06 -0700

    My C commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-	puts('"Programming is like building a multilingual puzzle\n');
+	char Mystr[] = "programming is like building a multilingual puzzle;
+	puts(Mystr);
 	return(0);
 }
