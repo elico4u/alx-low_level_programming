04d77f878aebe8dcc4f077abdcc5f89c8b1e8d95
Eli Amovure <eliamovure@gmail.com>
2022-10-17 20:16:25 -0700

    My C commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio>
+/**
+ *main - Entry point
+ *Return: Always o (Success)
+ */
+int main(void)
+{
+	int i;
+	double d;
+	char c;
+
+	printf("The size of an int is: %lu.\n", (unsigned long)sizeof(i));
+	printf("The size of an double is: %lu.\n", (unsigned long)sizeof(d));
+	printf("The size of an c is: %lu.\n", (unsigned long)sizeof(c));
+
+	return(0);
+}
