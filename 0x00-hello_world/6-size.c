e18fd1d823392616cfd813a9797d0dc89a9dd261
Eli Amovure <eliamovure@gmail.com>
2022-10-17 20:37:07 -0700

    My C commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,9 @@ int main(void)
 	int i;
 	double d;
 	char c;
-
-	printf("The size of an int is: %lu.\n", (unsigned long)sizeof(i));
-	printf("The size of an double is: %lu.\n", (unsigned long)sizeof(d));
-	printf("The size of an c is: %lu.\n", (unsigned long)sizeof(c));
+	print("Size of a char: %lu.\n", (unsigned long)sizeof(c) "byte(s)");
+	printf("Size of a int: %lu.\n", (unsigned long)sizeof(i) "bytes(s)");
+	printf("Size of a double: %lu.\n", (unsigned long)sizeof(&i) "bytes(s)");
 
 	return(0);
 }
