0c5680f8e649e41cf857538d4df5fc8526c30036
Eli Amovure <eliamovure@gmail.com>
2022-10-20 18:38:29 -0700

    task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if param. is a num btw 0 and 9
+ *  @c: input number
+ *  Return: 1 if it is a num (0 -9), 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if ((c >= 0) && (c <= 9))
+		return (1);
+	else
+		return (0);
+}
