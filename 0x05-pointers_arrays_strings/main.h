527d8253864f03d4d554d9142ba720ffc00c8e2d
Eli Amovure <eliamovure@gmail.com>
2022-10-24 13:45:45 -0700

    pointers and arrays strings project

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,6 +3,14 @@
 #ifndef MAIN_H
 
 void reset_to_98(int *n);
-
+wap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
 
 #endif
