f41b268e2c6a1e4abc1bca4163e257de22b5651e
Eli Amovure <eliamovure@gmail.com>
2022-10-24 14:14:07 -0700

    more functions nested loops project

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_numbers - prints nums between 0 to 9
- * Return: Always 0
+ * print_numbers - prints nums between 0123456789\n
+ * Return: void
  */
 
 void print_numbers(void)
